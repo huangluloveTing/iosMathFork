942a040402245dfb22673902a675fb8b90c01564
hl <huanglu@hldeMacBook-Pro.local>
2020-03-25 12:39:19 +0800

    update

--- iosMath/IosMath.h
+++ iosMath/IosMath.h
@@ -8,7 +8,7 @@
 //  This software may be modified and distributed under the terms of the
 //  MIT license. See the LICENSE file for details.
 
-#import <IosMath/MTMathUILabel.h>
-#import <IosMath/MTMathListDisplay.h>
-#import <IosMath/MTMathList.h>
-#import <IosMath/MTMathListBuilder.h>
+#import <iosMathFork/MTMathUILabel.h>
+#import <iosMathFork/MTMathListDisplay.h>
+#import <iosMathFork/MTMathList.h>
+#import <iosMathFork/MTMathListBuilder.h>
